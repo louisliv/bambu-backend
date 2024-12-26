7d53f2ccf64ff4295f2e8e789f5c10fc7be5e61e
fidoriel <49869342+fidoriel@users.noreply.github.com>
2024-12-26 23:13:02 +0100

    install svelte shadcn

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -2,8 +2,17 @@
     import PrinterSelection from "./lib/PrinterSelection.svelte";
     import Printer from "./lib/Printer.svelte";
     import { RouterView } from "@dvcol/svelte-simple-router/components";
+    import { ModeWatcher } from "mode-watcher";
+
+    import Sun from "lucide-svelte/icons/sun";
+    import Moon from "lucide-svelte/icons/moon";
+
+    import { toggleMode } from "mode-watcher";
+    import { Button } from "$lib/components/ui/button/index.js";
 
     import type { Route, RouterOptions } from "@dvcol/svelte-simple-router/models";
+
+    let { children } = $props();
 
     const RouteName = {
         Home: "home",
@@ -40,3 +49,11 @@
 </script>
 
 <RouterView {options} />
+<ModeWatcher />
+{@render children?.()}
+
+<Button onclick={toggleMode} variant="outline" size="icon">
+    <Sun class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+    <Moon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    <span class="sr-only">Toggle theme</span>
+</Button>
