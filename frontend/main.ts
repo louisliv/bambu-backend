37487111ff90d4699c3da849f201f4c3a86df75d
fidoriel <49869342+fidoriel@users.noreply.github.com>
2024-12-26 10:59:17 +0100

    work around crypto http insecure

--- frontend/main.ts
+++ frontend/main.ts
@@ -1,9 +1,14 @@
 import { mount } from "svelte";
 import "./app.css";
 import App from "./App.svelte";
+import { v4 as uuid4 } from "uuid";
 
 const app = mount(App, {
     target: document.getElementById("app")!,
 });
+
+window.crypto.randomUUID = () => {
+    return uuid4();
+};
 
 export default app;
